29b309d041b8efcfaaffc015280feed0894be2b0
omar abdi <o.hassanabdi@se18.qmul.ac.uk>
2019-10-03 13:13:41 +0100

    Updates

---
+++ precache-manifest.e0d21ac1019368676ead2e9fc5ac4323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f021c9e3d15d1376822334579d4897b",
+    "url": "/MonstersForHire/index.html"
+  },
+  {
+    "revision": "2ae5ac32a1fe5838e1ab",
+    "url": "/MonstersForHire/static/css/main.37187d87.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MonstersForHire/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2ae5ac32a1fe5838e1ab",
+    "url": "/MonstersForHire/static/js/main.b4e1d5ba.chunk.js"
+  },
+  {
+    "revision": "561a6da1f77273ad4c80",
+    "url": "/MonstersForHire/static/js/runtime~main.e62c7f40.js"
+  }
+]);
